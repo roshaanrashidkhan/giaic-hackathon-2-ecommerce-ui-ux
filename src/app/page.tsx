ce250b14557aa79b06cde9556380a3416acb5f67
roshaanrashidkhan <roshaan.rashid.r50@gmail.com>
2024-12-17 18:49:56 +0500

    vita classic section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       <main className="w-full">
         {visibility && <NavMenu pathname={pathname} />}
         <HeroSection
-          css="bg-[url('/carousel-inner.png')] bg-no-repeat bg-cover bg-[60%_0]"
+          css="bg-[#23A6F0] bg-[url('/carousel-inner.png')] bg-no-repeat bg-cover bg-[60%_0]"
           src=""
           alt=""
           firstHeading="Summer 2020"
@@ -48,11 +48,41 @@ export default function Home() {
               </Button>
             </div>
           }
-          paraCSS="text-xl w-full sm:w-[40%] text-center sm:text-left"
+          paraCSS="text-xl w-full text-center sm:text-left w-[70%]"
         />
         <EditorPick />
-        <FeatureSection headings={<><HeadingSection heading="Featured Products" css="text-gray-500" />
-          <Heading headingLg="BESTSELLER PRODUCTS" headingSm="Problems trying to resolve the conflict between" /></>} />
+        <FeatureSection
+          headings={
+            <>
+              <HeadingSection heading="Featured Products" css="text-gray-500" />
+              <Heading
+                headingLg="BESTSELLER PRODUCTS"
+                headingSm="Problems trying to resolve the conflict between"
+              />
+            </>
+          }
+        />
+        <HeroSection width={600} height={20}
+          css="bg-[#23856D]"
+          src="/png-image.png"
+          alt="Image"
+          firstHeading="Summer 2020"
+          secondHeading="Vita Classic Product"
+          para="We know how large objects will act, but things on a small scale."
+          jsxElement={
+            <div className="flex flex-col sm:flex-row items-center gap-5 sm:gap-20">
+              <h3 className="text-2xl font-bold">$16.48</h3>
+              <Button
+                variant="green"
+                size="lg"
+                className="w-max font-bold text-2xl"
+              >
+                SHOP NOW
+              </Button>
+            </div>
+          }
+          paraCSS="text-xl text-center sm:text-left"
+        />
         <SummerSection
           src="/grid-image.png"
           width={700}
@@ -64,7 +94,7 @@ export default function Home() {
       <Footer
         footerHeading={
           <FooterHeading
-          css="justify-start"
+            css="justify-start"
             heading="Bandage"
             content={
               <>
--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -16,15 +16,15 @@ interface Props {
 
 export default function HeroSection({css, firstHeading, secondHeading, para, jsxElement, src, alt, width, height, paraCSS}: Props) {
     return (
-        <section className={css + " w-full flex items-center justify-center min-h-screen bg-[#23A6F0] " }>
-            <div className="w-[85%] flex flex-col sm:flex-row items-center justify-center sm:justify-start py-3">
-                <div className="sm:w-full text-white flex flex-col sm:justify-start sm:text-left text-center justify-center gap-8">
+        <section className={css + " w-full flex items-center justify-center min-h-screen overflow-hidden pt-10" }>
+            <div className="w-[85%] h-full flex flex-col sm:flex-row items-center justify-center sm:justify-start gap-10">
+                <div className="text-white flex flex-col sm:justify-start sm:text-left text-center justify-center gap-8 w-full sm:w-[70%]">
                     <h6 className="font-bold text-base uppercase">{firstHeading}</h6>
-                    <h3 className="font-bold text-4xl  md:text-6xl">{secondHeading}</h3>
+                    <h3 className="font-bold text-4xl md:text-6xl">{secondHeading}</h3>
                     <p className={paraCSS}>{para}</p>
                     <div className="flex sm:block items-center justify-center">{jsxElement}</div>
                 </div>
-                {src.length > 0 && <Image src={src} objectFit="cover" layout="fill" width={width} height={height} alt={alt} />}
+                {src.length > 0 && <Image className="self-end -mb-10 w-full sm:w-[60%]" src={src} width={width} height={height} alt={alt} />}
             </div>
         </section>
     );
