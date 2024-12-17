81e26335c24bf3297444aa173972aae95816c2bc
roshaanrashidkhan <roshaan.rashid.r50@gmail.com>
2024-12-17 18:54:00 +0500

    image quality corrected

--- src/components/summer-section.tsx
+++ src/components/summer-section.tsx
@@ -12,7 +12,7 @@ export default function SummerSection({src, width, height, alt}: Props) {
     return (
         <section className="flex items-center justify-center w-full h-full">
             <div className="w-[85%] my-10 gap-x-4 gap-y-5 grid grid-rows-1 md:grid-cols-2 items-center justify-center">
-                <Image src={src} width={width} height={height} alt={alt} className="order-2 md:order-1" />
+                <Image src={src} width={width} height={height} alt={alt} className="order-2 md:order-1" quality={100} />
                 <div className="font-bold flex flex-col gap-5 order-1 justfy-center md:justify-start text-center md:text-left md:order-2">
                     <h6 className="text-gray-400 tracking-[0.1px]">SUMMER 2020</h6>
                     <h2 className="text-[40px] leading-[3rem] tracking-[0.2px]">Part of the Neural Universe</h2>
